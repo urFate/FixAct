4dfb98688b0730fa54ea09f1ac6c005c54fab000
ArnyFour <43936310+Arny4@users.noreply.github.com>
2020-03-10 23:41:56 +0300

    Testing Zone

--- Testing zone/OfficeLibDownload.py
+++ Testing zone/OfficeLibDownload.py
@@ -1,21 +1,45 @@
 import os
 import wget
+import time
+import zipfile
 
 '''
 About this file:
 ==============================
 This script loads external libraries for Office 2010 and Office 2013. 
-They are needed for activation through the KMS server, 
+They are needed for activation through the GMS server, 
 because Office 2010 and 2013 do not have these libraries by default.
 ==============================
 '''
 
 def download():
-    print("Downloading Office libs...")
-    lib13 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2013.zip?raw=true"
-    lib10 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2010.zip?raw=true"
-    wget.download(lib13)
-    wget.download(lib10)
+    try:
+        os.mkdir("C:/libraries")
+        os.mkdir("C:/libraries/office13")
+        input("Press any key to start...")
+        print("\nЗагрузка внешней библеотеки для активаци...")
+        lib13 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2013.zip?raw=true"
+        lib10 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2010.zip?raw=true"
+        out = "C:/libraries/"
+        wget.download(lib13, out=out)
+        wget.download(lib10, out=out)
+        libs = zipfile.ZipFile('C:/libraries/office2013.zip')
+        libs.extractall('C:/libraries/office13/')
+        time.sleep(3)
+        print("\nЗавершено!")
+    except OSError:
+        input("Press any key to start...")
+        print("\nЗагрузка внешней библеотеки для активаци...")
+        lib13 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2013.zip?raw=true"
+        lib10 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2010.zip?raw=true"
+        out = "C:/libraries/"
+        wget.download(lib13, out=out)
+        wget.download(lib10, out=out)
+        libs = zipfile.ZipFile('C:/libraries/office2013.zip')
+        libs.extractall('C:/libraries/office13/')
+        time.sleep(3)
+        print("\nЗавершено!")
+
 
 download()
 
