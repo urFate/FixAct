8f798138a5e8a71e9e613aa23657627d680ef8cc
ArnyFour <43936310+Arny4@users.noreply.github.com>
2020-03-10 20:21:03 +0300

    Test #2

---
+++ Testing zone/OfficeLibDownload.py
@@ -0,0 +1,22 @@
+import os
+import wget
+
+'''
+About this file:
+==============================
+This script loads external libraries for Office 2010 and Office 2013. 
+They are needed for activation through the GMS server, 
+because Office 2010 and 2013 do not have these libraries by default.
+==============================
+'''
+
+def download():
+    print("Downloading Office libs...")
+    lib13 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2013.zip?raw=true"
+    lib10 = "https://github.com/Arny4/FixAct/blob/master/OldOfficeVL_libs/office2010.zip?raw=true"
+    wget.download(lib13)
+    wget.download(lib10)
+
+download()
+
+input()
